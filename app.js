9f7dcfdacb516ac3aba3fa2dbd7f06e62af896a0
Se7eNss <oguzhanseven18@gmail.com>
2021-08-08 13:35:52 +0300

    v1

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express();
+const path = require ('path')
+
+const errorMiddleware = require('./middlewares/errors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const pin = require('./routes/pin');
+const user = require('./routes/user')
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(express.json());
+
+
+app.use(express.static(path.join(__dirname,"build")));
+app.get("/",(req,res,next)=>{
+    res.sendFile(path.join(__dirname,"build","index.html"))
+});
+
+app.use('/api/v1',pin);
+app.use('/api/v1',user);
+
+app.use(errorMiddleware);
+
+module.exports= app
